1cd3a09f9a51c12cf32b91ecf5d28f275c90932d
Otis Programmer <otisprogrammer@gmail.com>
2023-03-17 04:18:53 +0330

    add baseentity and value object to domain.

---
+++ Src/Services/Catalog/Core/Catalog.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Catalog.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime? UpdatedDateTime { get; set; }
+    }
+}
